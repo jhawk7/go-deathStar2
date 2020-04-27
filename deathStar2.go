1aea74de38e1a77e57b6973c6c6821e4e2cc96de
jhawk7 <jhawk154@gmail.com>
2020-04-27 19:53:34 -0400

    init

---
+++ deathStar2.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	retryablehttp "github.com/hashicorp/go-retryablehttp"
+	"net/http"
+	"github.com/pkg/errors"
+	"sync"
+)
+
+type Config struct {
+	ClientId string
+	Secret string
+	TokenUrl string
+	HttpClient *http.Client
+}
+
+const MAX_ROUTINES int = 10000
+
+func main() {
+	method := "GET"
+	body := []byte(nil)
+	url := "http://localhost:8080/healthcheck"
+
+	var wg sync.WaitGroup // create wait group (empty struct)
+
+	for i := 0; i < MAX_ROUTINES; i++ {
+		wg.Add(1)
+		go makeRequest(method, url, body, &wg)
+	}
+
+	wg.Wait()
+}
+
+var RequestHook = func(logger retryablehttp.Logger, req *http.Request, retryNumber int) {
+	fmt.Printf("Making Request to URL: %s; Retry Count: %v\n", req.URL, retryNumber)
+}
+
+var ResponseHook = func(logger retryablehttp.Logger, res *http.Response) {
+	fmt.Printf("URL: %v responded with Status: %v\n", res.Request.URL, res.StatusCode)
+}
+
+func makeRequest(method string, url string, body []byte, wg *sync.WaitGroup) {
+	retryClient := getClient()
+
+	request, _ := retryablehttp.NewRequest(method, url, bytes.NewBuffer(body))
+	request.Header.Set("Content-Type", "application/json")
+	response, requestErr := retryClient.Do(request)
+	if requestErr != nil {
+		err := errors.New(fmt.Sprintf("Error in request: %v\n", requestErr))
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(response.StatusCode)
+	wg.Done()
+}
+
+func getClient() *retryablehttp.Client {
+	retryClient := retryablehttp.NewClient()
+	retryClient.RequestLogHook = RequestHook
+	retryClient.ResponseLogHook = ResponseHook
+	retryClient.RetryMax = 3
+
+	return retryClient
+}
