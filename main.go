ca7aa58b405031d1af8667300b657f12b5817683
jhawk7 <13038876+jhawk7@users.noreply.github.com>
2025-11-26 16:51:36 -0500

    fixing retry client initialization

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ type Config struct {
 	RetryMax           int
 	TargetURL          string
 	Method             string
-	RequestBody        string
+	RequestBody        []byte
 	ExpectedStatusCode int
 }
 
@@ -66,10 +66,13 @@ func getConfig() *Config {
 		return nil
 	}
 
-	body, exists := os.LookupEnv("REQUEST_BODY")
+	bodystr, exists := os.LookupEnv("REQUEST_BODY")
 	if !exists {
+		bodystr = ""
 		logrus.Info("REQUEST_BODY not set, defaulting to empty body")
 	}
+
+	body := []byte(bodystr)
 
 	return &Config{
 		MaxRoutines: maxRoutines,
@@ -82,6 +85,11 @@ func getConfig() *Config {
 
 func main() {
 	config := getConfig()
+	if config == nil {
+		// getConfig already logged the fatal error, ensure we exit cleanly
+		os.Exit(1)
+	}
+
 	wg := sync.WaitGroup{}
 	for i := 1; i <= config.MaxRoutines; i++ {
 		logrus.Infof("Making request routine %v", i)
@@ -94,9 +102,11 @@ func main() {
 }
 
 func makeRequest(c *Config, index int, wg *sync.WaitGroup) {
-	//make request and log results; use some form of cancellation
+	// ensure wg.Done is called exactly once for this goroutine
+	defer wg.Done()
+
 	retryClient := getRetryClient(c)
-	req, reqErr := retryablehttp.NewRequest(c.Method, c.TargetURL, bytes.NewBuffer([]byte(c.RequestBody)))
+	req, reqErr := retryablehttp.NewRequest(c.Method, c.TargetURL, bytes.NewBuffer(c.RequestBody))
 	if reqErr != nil {
 		err := fmt.Errorf("error creating request: %v; index: %v", reqErr, index)
 		HandleError(err, false)
@@ -110,27 +120,25 @@ func makeRequest(c *Config, index int, wg *sync.WaitGroup) {
 	}
 
 	logrus.Infof("Request %v completed successfully", index)
-	wg.Done()
 }
 
 func getRetryClient(c *Config) *retryablehttp.Client {
-	retryClient := retryablehttp.Client{
-		HTTPClient: &http.Client{},
-		RetryMax:   c.RetryMax,
-		RequestLogHook: func(l retryablehttp.Logger, req *http.Request, count int) {
-			logrus.Info("Making request to URL: %v, Retry Count: %v", req.URL, count)
-		},
-		ResponseLogHook: func(l retryablehttp.Logger, res *http.Response) {
-			logrus.Info("URL: %v; Status: %v", res.Request.URL, res.StatusCode)
-		},
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = c.RetryMax
+	retryClient.RequestLogHook = func(l retryablehttp.Logger, req *http.Request, count int) {
+		logrus.Infof("Making request to URL: %v, Retry Count: %v", req.URL, count)
 	}
-	return &retryClient
+	retryClient.ResponseLogHook = func(l retryablehttp.Logger, res *http.Response) {
+		logrus.Infof("URL: %v; Status: %v", res.Request.URL, res.StatusCode)
+	}
+
+	return retryClient
 }
 
 func HandleError(err error, fatal bool) {
 	if err != nil {
 		if fatal {
-			logrus.Fatal()
+			logrus.Fatal(err)
 		} else {
 			logrus.Error(err)
 		}
